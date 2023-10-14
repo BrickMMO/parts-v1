c3116d5a7462f402737368570da8e303d1159544
Adam <thomasadam83@hotmail.com>
2023-10-14 19:31:42 -0400

    Added set page

--- theme.php
+++ theme.php
@@ -6,6 +6,18 @@ include('includes/functions.php');
 
 ?>
 
+<?php
+
+$query = 'SELECT * 
+    FROM themes
+    WHERE id = '.$_GET['id'].'
+    LIMIT 1';
+$result = mysqli_query($connect, $query);
+
+$theme = mysqli_fetch_assoc($result);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,35 +25,38 @@ include('includes/functions.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Home | BrickMMO Parts</title>
+    <title>Theme | BrickMMO Parts</title>
 
     <link href="styles.css" type="text/css" rel="stylesheet">
 
 </head>
 <body>
 
-    <h1>Themes</h1>
+    <h1><?=$theme['name']?></h1>
 
     <?php
 
     $query = 'SELECT * 
-        FROM themes
+        FROM sets
+        WHERE theme_id = '.$theme['id'].'
         ORDER BY name';
     $result = mysqli_query($connect, $query);
 
     ?>
 
-    <?php while($theme = mysqli_fetch_assoc($result)): ?>
+    <h2>Sets</h2>
 
-        <?=$theme['name']?>
+    <?php while($set = mysqli_fetch_assoc($result)): ?>
 
-        <br>
-
-        <a href="/theme.php?id=<?=$theme['id']?>">Theme Details</a>
+        <?=$set['name']?>
 
         <br>
 
-        <pre><?php print_r($theme); ?></pre>
+        <a href="/set.php?id=<?=$set['set_num']?>">Set Details</a>
+
+        <br>
+
+        <pre><?php print_r($set); ?></pre>
 
         <hr>
 
