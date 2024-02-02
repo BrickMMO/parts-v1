038b806f4f4ca9dc13f3c01c43f6b0e71bcea788
yashmodi998 <yashmodi998@gmail.com>
2024-02-02 11:05:56 -0500

    index php changes

--- index.php
+++ index.php
@@ -27,9 +27,11 @@ include('includes/header.php');
 
     ?>
 
+<div class="container text-center">
+  <div class="row">
     <?php while($theme = mysqli_fetch_assoc($result)): ?>
-
-        <hr>
+    
+       
         
         <?php
 
@@ -47,30 +49,48 @@ include('includes/header.php');
 
         ?>
 
-        <h3>Theme: <?=$theme['name']?></h3>
+        <!-- <h3>Theme: <?=$theme['name']?></h3>
 
         Set: <?=$set['name']?>
         <br>
-        Number: <?=$set['set_num']?>
+        Number: <?=$set['set_num']?> -->
 
-        <br><br>
+    
 
-        <a href="theme.php?id=<?=$theme['id']?>">Theme Details</a>
-
-        <br><br>
-
-        Full Theme Data:
-        <pre><?php print_r($theme); ?></pre>
+        <!-- Full Theme Data: -->
+        <!-- <pre><?php// print_r($theme); ?></pre> -->
         <?php
 
 ?>
-        Full Set Data:
-        <pre><?php print_r($set); ?></pre>
+        <!-- Full Set Data: -->
+        <!-- <pre><?php //print_r($set); ?></pre> -->
         <?php
-        echo $set['set_num'];
-        echo '<img  height=200px width=200px src="'.$set['img_url'].'"/></br>';  
- 
-        ?>
+        // echo $set['set_num'];
+        // echo '<img  height=200px width=200px src="'.$set['img_url'].'"/></br>'; 
+        ?> 
+        <div class="col">
+            <a href="theme.php?id=<?=$theme['id']?>">
+            <img class="img-thumbnai img-fluid"  height="200px" width="200px"  src=<?=$set['img_url'];?> alt="<?=$set['name'];?>">
+        </div>
     <?php endwhile; ?>
+    </div>
+    </div>
 <?php include('includes/footer.php');?>
 
+<!-- <div class="container">
+  <div class="row">
+  <div class="card-deck">
+<div class="col-sm">
+        <div class="card" style="width: 18rem;">
+        <a href="theme.php?id=<?=$theme['id']?>">
+        <img class="card-img-top img-fluid"    src=<?=$set['img_url'];?> alt="<?=$set['name'];?>">
+    </a>
+        <div class="card-body">
+        <h5 class="card-title"><?=$set['name'];?></h5>
+                           </div>
+      </div>
+    </div>
+
+    </div>
+    </div>
+</div> -->
