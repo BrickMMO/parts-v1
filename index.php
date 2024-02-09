a9f2a68736421c6b2f1ae2251af517480926a2ca
yashmodi998 <yashmodi998@gmail.com>
2024-02-09 10:54:57 -0500

    Css added and Home Page Data Set [index.html and style.css]

--- index.php
+++ index.php
@@ -6,78 +6,46 @@ include('includes/functions.php');
 
 define('PAGE_TITLE', 'Home');
 include('includes/header.php');
-
 ?>
-
-
-
-<h1>BrickMMO Parts</h1>
 
 <h2>Themes</h2>
 
 <?php
-
-/*
-    Run a query to fecth all the themes
-    */
+/* Run a query to fecth all the themes */
 $query = 'SELECT * 
         FROM themes
         ORDER BY name';
 $result = mysqli_query($connect, $query);
-
 ?>
 
-<div class="container">
-    <div class="row row-cols-3">
-        <?php while ($theme = mysqli_fetch_assoc($result)) : ?>
-
-
-
-            <?php
-
-            /*
-        Select one set from the current theme
-        */
+<div class="container mt-4">
+    <div class="row">
+        <?php
+        while ($theme = mysqli_fetch_assoc($result)) :
+            /* Select one set from the current theme */
             $query = 'SELECT * 
             FROM sets
             WHERE theme_id = ' . $theme['id'] . '
             ORDER BY name
             LIMIT 1';
             $result2 = mysqli_query($connect, $query);
-
-            $set = mysqli_fetch_assoc($result2)
-
-            ?>
-
-            <!-- <h3>Theme: <?= $theme['name'] ?></h3>
-
-        Set: <?= $set['name'] ?>
-        <br>
-        Number: <?= $set['set_num'] ?> -->
-
-
-
-            <!-- Full Theme Data: -->
-            <!-- <pre><?php // print_r($theme); 
-                        ?></pre> -->
-            <?php
-
-            ?>
-            <!-- Full Set Data: -->
-            <!-- <pre><?php //print_r($set); 
-                        ?></pre> -->
-            <?php
-            if ($set['img_url']) {
-            ?>
-
-                <div class="col">
-                    <a href="theme.php?id=<?= $theme['id'] ?>">
-                        <img class="rounded mx-auto d-block" height="200px" width="200px" src=<?= $set['img_url']; ?> alt="<?= $set['name']; ?>">
+            if (mysqli_num_rows($result2) > 0) {
+                $set = mysqli_fetch_assoc($result2)
+        ?>
+                <div class="col-3 mb-4">
+                    <a class="text-decoration-none mb-4" href="theme.php?id=<?= $theme['id'] ?>">
+                        <div class="card justify-content-center" style="width:300px; height:370px; overflow: hidden;">
+                            <div class="parts-card-img-container p-2">
+                                <img class="rounded mx-auto d-block " src=<?= $set['img_url']; ?> alt="<?= $set['name']; ?>">
+                            </div>
+                            <div class="card-body" style="height: 50%; overflow-y:auto">
+                                <h3 class="card-title" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis; "><?= $theme['name']; ?></h3>
+                                <h6 class="card-subtitle text-muted">Set: <?= $set['name']; ?></h6>
+                                <p class="card-text">Number: <?= $set['set_num']; ?></p>
+                                </br>
+                            </div>
+                        </div>
                     </a>
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $set['name']; ?></h5>
-                        </br>
-                    </div>
                 </div>
         <?php
             }
