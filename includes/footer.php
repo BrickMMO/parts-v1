dda075ee27e3f832a83925084758cfcca636b053
khushi patel <khuship6126@gmail.com>
2024-03-08 09:34:19 -0500

    Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
-<div>footer</div>
-</body>
-</html>
+<footer class="footer mt-auto py-3 parts-footer">
+    <div class="container">
+        <span class="text parts-text">&copy;2024. All rights reserved</span>
+    </div>
+</footer>
